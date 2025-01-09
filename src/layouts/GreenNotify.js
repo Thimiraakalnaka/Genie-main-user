aa43d7ffd53e04a698c38bbd83721c205f18867a
Thimiraakalnaka <thimiraakalanka13@icloud.com>
2025-01-09 15:59:07 +0530

    register use form validate and intigrate with api and green notification show after succes full login

---
+++ src/layouts/GreenNotify.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+
+
+function GreenNotify({successMsg,setSuccessMsg }) {
+    const [open, setOpen] = useState(false);
+
+
+    useEffect(()=>{
+      if(successMsg){
+        setSuccessMsg(successMsg);
+        setOpen(true);
+        const timer = setTimeout(() => {
+          setOpen(false);
+          setSuccessMsg("");
+        }, 3000);
+        return () => clearTimeout(timer);
+      }
+
+    },[successMsg,setSuccessMsg])
+
+    return (
+      <Dialog open={open} onClose={() => setOpen(false)} className="relative z-10">
+          <div className="fixed top-5 right-5 z-50 w-full max-w-sm overflow-hidden">
+            <DialogPanel
+              transition
+              className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+            >
+              <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:size-10">
+                    <CheckCircleOutlineIcon aria-hidden="true" className="size-7 text-green-600" />
+                  </div>
+                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <DialogTitle as="h3" className="text-base font-semibold text-gray-900">
+                      {successMsg}
+                    </DialogTitle>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-green-500 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <button
+                  type="button"
+                  data-autofocus
+                  onClick={() => {setOpen(false);
+                    setSuccessMsg('');
+                  }}
+                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                >
+                  Close
+                </button>
+              </div>
+            </DialogPanel>
+          </div>
+      </Dialog>
+    )
+}
+
+export default GreenNotify
--- src/layouts/Register.js
+++ src/layouts/Register.js
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react';
 import Logo from '../assets/logo.jpg';
+import Axios from 'axios';
+import GreenNotify from './GreenNotify';
 
 const Register = ({open, setOpen, toggleToLogin}) => {
 
+  const[fname,setFname]=useState("");
+  const[lname,setLname]=useState("");
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+  const[cpassword,setCpassword]=useState("");
+  const[showFormMsg,setShowFormMsg]=useState({
+    fname : "",
+    lname : "",
+    email : "",
+    password: ""
+  });
+  const[successMsg,setSuccessMsg]=useState("");
+
+
+  const registerUser = () => {
+    if(validate()){
+  
+      const payload = {
+        firstname: fname,
+        lastname: lname,
+        email: email,
+        password: password
+      }
+  
+      Axios.post('http://localhost:8080/api/v1/adduser',payload)
+        .then(()=>{
+          clearForm();
+          setTimeout(() => toggleToLogin(true), 3000);
+          setSuccessMsg("Registration succuessfull !")
+        }).catch((error)=>{
+          console.log("Axios error user regiter : ", error);
+        })
+
+    }
+
+    
+  }
+
+
+  function validate(){
+
+    const newFormMsg ={ fname:'',lname:'',email:'',password:'',cpassword:''};
+    const regex=/\S+@\S+\.\S+/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/;
+
+    let isVlaid = true;
+
+    if(!fname){
+      newFormMsg.fname = "First name is requird";
+      isVlaid = false;
+    }
+
+    if(!lname){
+      newFormMsg.lname = "Last name is requird";
+      isVlaid = false;
+    }
+
+    if(!email){
+      newFormMsg.email = "Email is requird";
+      isVlaid = false;
+    }else if(!regex.test(email)){
+      newFormMsg.email = "Email format invalid";
+      isVlaid = false;
+    }
+
+    if(!password){
+      newFormMsg.password = "password is requird";
+      isVlaid = false;
+    }else if(password.length < 8){
+      newFormMsg.password = "Password must be at least 8 characters";
+      isVlaid = false;
+    }
+    else if(!passwordRegex.test(password)){
+      newFormMsg.password= "include uppercase, lowercase, a number, and a special character.";
+      isVlaid = false;
+    }
+
+    if(!cpassword){
+      newFormMsg.cpassword = "Confirm password is required";
+      isVlaid = false;
+    }
+    else if(password !== cpassword){
+      newFormMsg.cpassword = "password do not match";
+      isVlaid = false;
+    }
+    setShowFormMsg(newFormMsg);
+    return isVlaid;
+  }
+
+  function clearForm(){
+    setFname('');
+    setLname('');
+    setEmail('');
+    setPassword('');
+    setCpassword('');
+  }
 
   return (
     <div>
@@ -16,7 +114,7 @@ const Register = ({open, setOpen, toggleToLogin}) => {
           <div className="flex min-h-full items-end justify-center p-4 text-center">
             <DialogPanel
               transition
-              className="relative transform overflow-hidden w-[400px] rounded-[50px] bg-white border-[#06B2F6] border-4 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+              className="relative transform overflow-hidden w-[450px] rounded-[50px] bg-white border-[#06B2F6] border-4 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
             >
               <div className="bg-white px-4 py-5">
                
@@ -31,12 +129,22 @@ const Register = ({open, setOpen, toggleToLogin}) => {
                   </DialogTitle>
                   <div className="grid py-5 mx-6">
                     <div className='flex gap-2'>
-                    <input className="bg-[#D9D9D9] my-2 h-8 w-[50%] rounded-md" placeholder="First Name" />
-                    <input className="bg-[#D9D9D9] my-2 h-8 w-[50%] rounded-md" placeholder="Last Name" />
+                      <div className='flex flex-col w-[50%]'>
+                      <input className="bg-[#D9D9D9] my-2 h-8 rounded-md" placeholder="First Name" value={fname} onChange={(e)=>{setFname(e.target.value)}} />
+                      <p className='text-red-600 text-left'>{showFormMsg.fname}</p>
+                      </div>
+                      <div className='flex flex-col w-[50%]'>
+                      <input className="bg-[#D9D9D9] my-2 h-8 rounded-md" placeholder="Last Name" value={lname} onChange={(e)=>{setLname(e.target.value)}} />
+                      <p className='text-red-600 text-left'>{showFormMsg.lname}</p>
+                      </div>
+                      
                     </div>
-                    <input className="bg-[#D9D9D9] my-2 h-8 rounded-md" placeholder="E-mail" />
-                    <input className="bg-[#D9D9D9] my-2 h-8 rounded-md" placeholder="Password" />
-                    <input className="bg-[#D9D9D9] my-2 h-8 rounded-md" placeholder="Confirm Password" />
+                    <input className="bg-[#D9D9D9] my-2 h-8 rounded-md" placeholder="E-mail" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
+                    <p className='text-red-600 text-left'>{showFormMsg.email}</p>
+                    <input className="bg-[#D9D9D9] my-2 h-8 rounded-md" placeholder="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                    <p className='text-red-600 text-left'>{showFormMsg.password}</p>
+                    <input className="bg-[#D9D9D9] my-2 h-8 rounded-md" placeholder="Confirm Password" value={cpassword} onChange={(e)=>{setCpassword(e.target.value)}}/>
+                    <p className='text-red-600 text-left'>{showFormMsg.cpassword}</p>
                     <p className="font-medium text-[9px] text-gray-500 text-left mt-4">
                       By selecting Create personal account, you agree to our User Agreement and acknowledge
                       reading our User Privacy Notice.
@@ -45,6 +153,7 @@ const Register = ({open, setOpen, toggleToLogin}) => {
                   <div>
                     <button
                       className="bg-[#8DD4F1] w-48 h-6 text-center font-bold rounded-3xl mb-3"
+                      onClick={registerUser}
                     >
                       Register
                     </button>
@@ -52,7 +161,7 @@ const Register = ({open, setOpen, toggleToLogin}) => {
                       Already have an account?{' '}
                       <span
                         className="text-[#06B2F6] font-bold cursor-pointer"
-                        onClick={() => toggleToLogin(true)} // Navigate back to login
+                        onClick={() => toggleToLogin(true)}
                       >
                         Sign in
                       </span>
@@ -64,6 +173,7 @@ const Register = ({open, setOpen, toggleToLogin}) => {
           </div>
         </div>
       </Dialog>
+     {successMsg && <GreenNotify successMsg={successMsg} setSuccessMsg={setSuccessMsg} />}
     </div>
   )
 }
